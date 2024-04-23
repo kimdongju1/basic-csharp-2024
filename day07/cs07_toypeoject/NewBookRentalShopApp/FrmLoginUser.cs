c20b187d54fd7d23ed4895eede0b30940ed13efa
kimdongju1 <dkdlkdh@naver.com>
2024-04-23 09:02:52 +0900

    .

---
+++ day07/cs07_toypeoject/NewBookRentalShopApp/FrmLoginUser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace NewBookRentalShopApp
+{
+    public partial class FrmLoginUser : MetroForm
+    {
+        private string connString = "Data Source=localhost;" +
+                                    "Initial Catalog=BookRentalShop2024;" +
+                                    "Persist Security Info=True;" +
+                                    "User ID=sa;Encrypt=False;Password=mssql_p@ss";
+        public FrmLoginUser()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLoginUser_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+                var query = @"SELECT userIdx
+                                   , userId
+                                   , [password]
+                                   , lastLoginDateTime
+                                FROM usertbl";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "usertbl");
+
+                DgvResult.DataSource = ds.Tables[0];
+                DgvResult.ReadOnly = true;  // 수정불가
+                DgvResult.Columns[0].HeaderText = "사용자순번";
+                DgvResult.Columns[1].HeaderText = "사용자아이디";
+                DgvResult.Columns[2].HeaderText = "패스워드";
+                DgvResult.Columns[3].HeaderText = "마지막로그인날짜";
+            }
+        }
+    }
+}
